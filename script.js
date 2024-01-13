3987299fe0e2795539adff48e0308d81554579df
Irfan khan <80601941+ik-deep@users.noreply.github.com>
2024-01-13 10:51:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,33 @@
+ function isPalindrome(num) {
+      // Convert the number to a string
+      var numStr = num.toString();
+
+      // Reverse the string
+      var reversedStr = numStr.split('').reverse().join('');
+
+      // Check if the original and reversed strings are the same
+      return numStr === reversedStr;
+    }
+
+
+
 function nextPalindrome(num) {
   //your JS code here.
+	 // var number = parseFloat(num);
+
+      // Check if the input is a valid number
+      // if (isNaN(number)) {
+      //   alert("Please enter a valid number.");
+      //   return;
+      // }
+
+      // Find the next palindrome number
+      do {
+        num++;
+      } while (!isPalindrome(num));
+
+	return num;
+
 }
 
 const input = prompt("Enter a palindrome number");
